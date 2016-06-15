64a888ca75bb192f9c467f19121a075311dad1b6
jeffryporter <jporter7@gmail.com>
2016-06-15 11:57:49 -0400

    Add Chat

---
+++ src/com/theironyard/javawithclojure/jhporter/Message.java
@@ -0,0 +1,52 @@
+package com.theironyard.javawithclojure.jhporter;
+
+/**
+ * Created by jeffryporter on 6/15/16.
+ */
+public class Message
+{
+    int id;
+    String author;
+    String text;
+
+    public Message()
+    {
+    }
+
+    public Message(int id, String author, String text)
+    {
+        this.id = id;
+        author = author;
+        this.text = text;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getAuthor()
+    {
+        return author;
+    }
+
+    public void setAuthor(String author)
+    {
+        this.author = author;
+    }
+
+    public String getText()
+    {
+        return text;
+    }
+
+    public void setText(String text)
+    {
+        this.text = text;
+    }
+}
